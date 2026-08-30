298647a25ae01cf8e34edd86263020425292eecd
rubanik.roman <rubanik.roman@wb.ru>
2026-08-30 23:58:23 +0300

    add regular for log parsing

--- main.go
+++ main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+	"time"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+type LogEntry struct {
+	Timestamp time.Time
+	Level     string
+	Service   string
+	Message   string
+	RequestID string
+	UserID    string
+}
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+func main() {
+	logExpample := "2023-12-25T14:30:15.123Z [INFO] user-service: User authenticated, request_id=req_abc123, user_id=12345"
+
+	entry, err := ParseLogLine(logExpample)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println("Parsed Log Entry:", entry)
+}
+
+func ParseLogLine(line string) (LogEntry, error) {
+	pattern := regexp.MustCompile(`^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)\s+\[(INFO|WARNING)]\s+(\S+):\s([^,]+),\s+request_id=([^,]+),\s+user_id=(\S+)$`)
+	matches := pattern.FindStringSubmatch(line)
+
+	timestamp, err := time.Parse(time.RFC3339, matches[1])
+	if err != nil {
+		return LogEntry{}, fmt.Errorf("failed to parse timestamp: %w", err)
+	}
+
+	return LogEntry{
+		Timestamp: timestamp,
+		Level:     matches[2],
+		Service:   matches[3],
+		Message:   matches[4],
+		RequestID: matches[5],
+		UserID:    matches[6],
+	}, nil
 }
